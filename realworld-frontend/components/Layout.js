fb13ad88dbc5181ddf6252b31a81bd9045e5fc46
kameno3629 <blueocean777k@gmail.com>
2024-11-20 00:26:24 +0900

    [edit_stylesheet_rendering]_document.js

--- realworld-frontend/components/Layout.js
+++ realworld-frontend/components/Layout.js
@@ -2,22 +2,13 @@
    import Head from 'next/head';
 
    export default function Layout({ children }) {
+     const userImageSrc = "/smiley-cyrus.jpeg";
+
      return (
        <>
          <Head>
            <meta charSet="utf-8" />
            <title>Conduit</title>
-           <link
-             href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
-             rel="stylesheet"
-             type="text/css"
-           />
-           <link
-             href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
-             rel="stylesheet"
-             type="text/css"
-           />
-           <link rel="stylesheet" href="//demo.productionready.io/main.css" />
          </Head>
          <nav className="navbar navbar-light">
            <div className="container">
@@ -38,7 +29,7 @@
                </li>
                <li className="nav-item">
                  <a className="nav-link" href="/profile/eric-simons">
-                   <img src="" className="user-pic" alt="User" />
+                   <img src={userImageSrc} className="user-pic" alt="User" />
                    Eric Simons
                  </a>
                </li>
